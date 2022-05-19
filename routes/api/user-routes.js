66d64c8c95578b603fcc18df229cc9e22e4d574c
Robert Jimenez <robertjimenez86@gmail.com>
2022-05-19 17:56:25 -0500

    added login credential check

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -37,7 +37,7 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    // expects {username: 'albert', email: 'albert@gmail.com', password: 'password321'}
     User.create({
       username: req.body.username,
       email: req.body.email,
@@ -48,7 +48,29 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+router.post('/login', (req, res) => {
+  // expects {username: 'albert', email: 'albert@gmail.com', password: 'password321'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({message: 'No user with that email address!'});
+    }
+    // res.json({user:dbUserData});
+    // verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword){
+      res.status(400).json({message: 'Incorrect password!'})
+      return;
+    }
+    res.json({user: dbUserData, message: 'You are now logged in!'});
   });
+});
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
