bca20683d32be54e296ce94719fa0c2eb6a432e9
Robert Jimenez <robertjimenez86@gmail.com>
2022-05-22 20:51:39 -0500

    added Comment model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 const Vote = require('./Vote');
 
 User.hasMany(Post, {
@@ -14,7 +15,7 @@ User.belongsToMany(Post, {
     through: Vote,
     as: 'voted_posts',
     foreignKey: 'user_id'
-  });
+});
   
 Post.belongsToMany(User, {
     through: Vote,
@@ -38,4 +39,20 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
