670d51e719056550afc683b0f9ba864dd5c1b3db
Robert Jimenez <robertjimenez86@gmail.com>
2022-05-30 15:48:13 -0500

    added logic to remove comment/vote when not logged in

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -11,7 +11,12 @@ router.get("/", (req, res) => {
       "post_url",
       "title",
       "created_at",
-      [sequelize.literal("(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"),"vote_count",],
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
     ],
     order: [["created_at", "DESC"]],
     include: [
@@ -96,13 +101,19 @@ router.post("/", (req, res) => {
 });
 
 router.put("/upvote", (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote, Comment, User })
-    .then((updatedVoteData) => res.json(updatedVoteData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote(
+      { ...req.body, user_id: req.session.user_id },
+      { Vote, Comment, User }
+    )
+      .then((updatedVoteData) => res.json(updatedVoteData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put("/:id", (req, res) => {
